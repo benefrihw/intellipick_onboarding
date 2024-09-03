337359b46e936eb47b33b95a2b0bc6f680d59f98
benefrihw <gjtja7@naver.com>
2024-09-04 00:06:53 +0900

    feat/로그인 구현

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import jwt from 'jsonwebtoken'
 
 dotenv.config();
 
@@ -7,40 +8,71 @@ const app = express();
 app.use(express.json());
 
 const PORT = process.env.PORT;
-const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY;
+const ACCESS_TOKEN_SECRET_KEY = process.env.ACCESS_TOKEN_SECRET_KEY;
+const REFRESH_TOKEN_SECRET_KEY = process.env.REFRESH_TOKEN_SECRET_KEY;
 
 // 임시 user 등록
 const users = [
   {
-    "username": "JIN HO",
-    "password": "12341234",
-    "nickname": "Mentos"
-  }
-]
+    username: "JIN HO",
+    password: "12341234",
+    nickname: "Mentos",
+  },
+];
 
 // accessToken 발급
-const accessToken = user => {
+const accessToken = (user) => {
   const payload = {
     username: user.username,
     nickname: user.nickname,
-    authorities: [{ authorityName: 'ROLE_USER'}],
+    authorities: [{ authorityName: "ROLE_USER" }],
   };
-  return jwt.sign(payload, JWT_SECRET_KEY, { expiresIn: '5m' });
-}
+  return jwt.sign(payload, ACCESS_TOKEN_SECRET_KEY, { expiresIn: "5m" });
+};
+
+// refreshToken 발급
+const refreshToken = (user) => {
+  const payload = {
+    username: user.username,
+    nickname: user.nickname,
+  };
+  return jwt.sign(payload, REFRESH_TOKEN_SECRET_KEY, { expiresIn: "1d" });
+};
 
 // 통신 확인
-app.get('/', (req, res) => {
-  res.json('Hello')
-})
+app.get("/", (req, res) => {
+  res.json("Hello");
+});
 
 // 회원가입
-app.post('/sign-up', (req, res) => {
+app.post("/signup", (req, res) => {
   const { username, password, nickname } = req.body;
 
   const user = { username, password, nickname };
   users.push(user);
 
-  res.json({ username, nickname, authorities: [{ authorityName: 'ROLE_USER '}]});
+  return res.json({
+    username,
+    nickname,
+    authorities: [{ authorityName: "ROLE_USER " }],
+  });
+});
+
+// 로그인
+app.post("/login", (req, res) => {
+  const { username, password } = req.body;
+
+  const user = users.find(
+    (user) => user.username === username && user.password === password
+  );
+  if (!user) {
+    return res.json({ message: "존재하지 않는 유저입니다." });
+  }
+
+  const userAccessToken = accessToken(user);
+  const userRefreshToken = refreshToken(user);
+
+  return res.json({ userAccessToken, userRefreshToken });
 });
 
 app.listen(PORT, () => {
